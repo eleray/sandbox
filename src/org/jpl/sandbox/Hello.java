abeee3263b2a54c7ed407fe5c5953e1da89a8911
eleray <eric.leray@jpl.ch>
2011-04-29 11:51:54 +0200

    another commit

--- src/org/jpl/sandbox/Hello.java
+++ src/org/jpl/sandbox/Hello.java
@@ -2,6 +2,6 @@ package org.jpl.sandbox;
 
 public class Hello {
   public static void main(String[] args) {
-    System.out.println("hello world");
+    System.out.println("hello world again");
   }
 }
